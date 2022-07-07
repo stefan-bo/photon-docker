591190d3e0644865de2f9e4625350959b8edc8d7
stefan-bo <55791185+stefan-bo@users.noreply.github.com>
2022-07-07 09:48:06 +0200

    Added download for germany only

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,10 @@ if [ ! -d "/photon/photon_data/elasticsearch" ]; then
 
     # Let graphhopper know where the traffic is coming from
     USER_AGENT="docker: thomasnordquist/photon-geocoder"
-    wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x
+    # Download whole planet
+    # wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x
+    # Download only Germany
+    wget --user-agent="$USER_AGENT" -O - https://download1.graphhopper.com/public/extracts/by-country-code/de/photon-db-de-latest.tar.bz2 | bzip2 -cd | tar x
 fi
 
 # Start photon if elastic index exists
