47f28f5e985b75548cfe3b931e1e13a450a6fe9c
stefan-bo <55791185+stefan-bo@users.noreply.github.com>
2022-07-07 10:39:10 +0200

    Set ram and cors

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ fi
 # Start photon if elastic index exists
 if [ -d "/photon/photon_data/elasticsearch" ]; then
     echo "Start photon"
-    java -jar photon.jar $@
+    java -Xms2g -Xmx2g -jar photon.jar -cors-any $@
 else
     echo "Could not start photon, the search index could not be found"
 fi
